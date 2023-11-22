4adfeccd1cfbce6232e032b4a22ffa71ee7daf6c
Farrukh <farrukh1994@naver.com>
2023-11-23 01:07:13 +0900

    main.js

--- project/main.js
+++ project/main.js
@@ -58,3 +58,113 @@ const topLine = Bodies.rectangle(310, 120, 630, 5, {
   let currentFruit = null;
   let disableAction = false;
   let interval = null;
+
+  function addFruit() {
+    const index = Math.floor(Math.random() * 5);
+    const fruit = FRUITS[index];
+
+    const body = Bodies.circle(300,50, fruit.radius, {
+        index: index,
+        issSleeping: true,
+        render: {
+            sprite: { texture: `${fruit.name}.png` }
+        },
+        restitution: 0.2,
+    });
+    
+    currentBody = body;
+    currentFruit = fruit;
+
+    World.add(world,body);
+  }
+
+window.onkeydown = (event) => {
+  if (disableAction) {
+    return;
+  }
+
+  switch (event.code) {
+    case "KeyA":
+      if (interval)
+        return;
+
+     interval = setInterval(() => {
+       if (currentBody.position.x - currentFruit.radius > 30 )
+         Body.setPosition(currentBody, {
+           x: currentBody.position.x - 1,
+           y: currentBody.position.y,
+         }); 
+     }, 5);
+     break;
+
+    case "KeyD":
+      if (interval)
+        return;
+        
+      interval = setInterval( () => {
+        if (currentBody.position.x + currentFruit.radius < 590)
+        Body.setPosition(currentBody, {
+           x: currentBody.position.x + 1,
+           y: currentBody.position.y,
+        });
+     }, 5); 
+     break;
+
+    case "KeyS":
+      currentBody.issSleeping = false;
+      disableAction = true;
+       
+      setTimeout(() => {
+        addFruit();
+        disableAction = false;  
+      }, 1000);
+      break;
+    }
+}
+
+window.onkeyup = (event) => {
+    switch (event.code) {
+      case "KeyA":
+      case "KeyD":
+        clearInterval(interval);
+        interval = null;
+    } 
+}
+
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    if (collision.bodyA.index === collision.bodyB.index) {
+      const index = collision.bodyA.index;
+      
+      if (index === FRUITS.length - 1) {
+        return;
+      }
+
+      World.remove(world, [collision.bodyA, collision.bodyB]);
+     
+      const newFruit = FRUITS[index + 1];
+
+      const newBody = Bodies.circle(
+        collision.collision.supports[0].x,
+        collision.collision.supports[0].y,
+        newFruit.radius,
+        {
+          render: {
+            sprite: { texture: `${newFruit.name}.png` }
+          },
+          index: index + 1,
+        }
+      );
+     
+      World.add(world, mewBody);
+    }
+
+    if (
+      !disableAction &&
+      (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
+      alert("Game over");   
+    }
+ }); 
+});
+
+  addFruit();

3e3d883ffba10af5e3cef8d2c06727c1b5ba1a17
Djon7 <dz45942@Gmail.com>
2023-12-07 03:55:13 +0900

    main page 개선

--- project/main.js
+++ project/main.js
@@ -1,4 +1,4 @@
-import { Bodies,Body,Engine,Events,Render,Runner,World } from "matter-js";
+import { Bodies, Body, Engine, Events, Render, Runner, World } from "matter-js";
 import { FRUITS_BASE, FRUITS_HLW } from "./fruits";
 import "./dark.css";
 
@@ -6,77 +6,104 @@ let THEME = "halloween"; // { base, halloween }
 let FRUITS = FRUITS_BASE;
 
 switch (THEME) {
-    case "halloween":
+  case "halloween":
     FRUITS = FRUITS_HLW;
     break;
-    default:
-        FRUITS = FRUITS_BASE;
+  default:
+    FRUITS = FRUITS_BASE;
 }
 
 const engine = Engine.create();
 const render = Render.create({
-    engine,
-    element: document.body,
-    options: {
-        wireframes: false,
-        background: "#F7F4C8",
-        width: 620,
-        height: 850,
-    }
+  engine,
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: "#F7F4C8",
+    width: 620,
+    height: 850,
+  }
 });
 
 const world = engine.world;
 
 const leftWall = Bodies.rectangle(15, 395, 30, 790, {
-    isStatic: true,
-    render: { fillStyle: "#E6B143" }
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
 });
 
 const rightWall = Bodies.rectangle(605, 395, 30, 790, {
-    isStatic: true,
-    render: { fillStyle: "#E6B143"}
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
 });
 
 const ground = Bodies.rectangle(310, 820, 620, 60, {
-    isStatic: true,
-    render: { fillStyle: "#E6B143" }
+  isStatic: true,
+  render: { fillStyle: "#E6B143" }
 });
 
 const topLine = Bodies.rectangle(310, 120, 630, 5, {
-    name: "topLine",
-    isStatic: true,
-    isSensor: true,
-    render: { fillStyle: "#E6B143" }
-  })
+  name: "topLine",
+  isStatic: true,
+  isSensor: true,
+  render: { fillStyle: "#E6B143" }
+})
 
-  World.add(world, [leftWall, rightWall, ground, topLine]);
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
-  Render.run(render);
-  Runner.run(engine);
+Render.run(render);
+Runner.run(engine);
 
-  let currentBody = null;
-  let currentFruit = null;
-  let disableAction = false;
-  let interval = null;
+let currentBody = null;
+let currentFruit = null;
+let disableAction = false;
+let interval = null;
 
-  function addFruit() {
-    const index = Math.floor(Math.random() * 5);
-    const fruit = FRUITS[index];
 
-    const body = Bodies.circle(300,50, fruit.radius, {
-        index: index,
-        isSleeping: true,
-        render: {
-            sprite: { texture: `${fruit.name}.png` }
-        },
-        restitution: 0.2,
-    });
-    
-    currentBody = body;
-    currentFruit = fruit;
 
-    World.add(world, body);
+
+
+
+
+
+
+
+function addFruit() {
+
+  function seededRandom(seed) {
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
   }
+  
+  var seed = 42; // 시드 값
+  var random1 = seededRandom(seed); // 시드로부터 생성된 첫 번째 난수
+  var random2 = seededRandom(seed); // 시드로부터 생성된 두 번째 난수
+  
+  console.log(random1, random2);
+
+
+
+
+
+  const index = Math.floor(Math.random() * 5);
+
+
+  const fruit = FRUITS[index];
+
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
+    isSleeping: true,
+    render: {
+      sprite: { texture: `${fruit.name}.png` }
+    },
+    restitution: 0.2,
+  });
+
+  currentBody = body;
+  currentFruit = fruit;
+
+  World.add(world, body);
+}
 
 window.onkeydown = (event) => {
   if (disableAction) {
@@ -88,62 +115,70 @@ window.onkeydown = (event) => {
       if (interval)
         return;
 
-     interval = setInterval(() => {
-       if (currentBody.position.x - currentFruit.radius > 30)
-         Body.setPosition(currentBody, {
-           x: currentBody.position.x - 1,
-           y: currentBody.position.y,
-         }); 
-     }, 5);
-     break;
+      interval = setInterval(() => {
+        if (currentBody.position.x - currentFruit.radius > 30)
+          Body.setPosition(currentBody, {
+            x: currentBody.position.x - 1,
+            y: currentBody.position.y,
+          });
+      }, 5);
+      break;
 
     case "KeyD":
       if (interval)
         return;
-        
+
       interval = setInterval(() => {
         if (currentBody.position.x + currentFruit.radius < 590)
         Body.setPosition(currentBody, {
-           x: currentBody.position.x + 1,
-           y: currentBody.position.y,
+          x: currentBody.position.x + 1,
+          y: currentBody.position.y,
         });
-     }, 5); 
-     break;
+      }, 5);
+      break;
 
     case "KeyS":
       currentBody.isSleeping = false;
       disableAction = true;
-       
+
       setTimeout(() => {
         addFruit();
-        disableAction = false;  
+        disableAction = false;
       }, 1000);
       break;
-    }
+  }
 }
 
 window.onkeyup = (event) => {
-    switch (event.code) {
-      case "KeyA":
-      case "KeyD":
-        clearInterval(interval);
-        interval = null;
-    } 
+  switch (event.code) {
+    case "KeyA":
+    case "KeyD":
+      clearInterval(interval);
+      interval = null;
+  }
+
+
 }
 
 Events.on(engine, "collisionStart", (event) => {
   event.pairs.forEach((collision) => {
     if (collision.bodyA.index === collision.bodyB.index) {
       const index = collision.bodyA.index;
-      
+
       if (index === FRUITS.length - 1) {
         return;
       }
 
       World.remove(world, [collision.bodyA, collision.bodyB]);
-     
-      const newFruit = FRUITS[index + 1];
 
+
+
+
+      const newFruit = FRUITS[index + 1];
+      const scoreElement = document.getElementById("score");
+      let score = 0;
+      
+      // Assuming you're using Matter.js to create a newBody
       const newBody = Bodies.circle(
         collision.collision.supports[0].x,
         collision.collision.supports[0].y,
@@ -155,16 +190,39 @@ Events.on(engine, "collisionStart", (event) => {
           index: index + 1,
         }
       );
-     
-      World.add(world, mewBody);
+      
+      // Update the score when a collision occurs
+      Events.on(newBody, "collisionStart", function (event) {
+        const pairs = event.pairs;
+        for (let i = 0; i < pairs.length; i++) {
+          const pair = pairs[i];
+          if (pair.bodyA === newBody || pair.bodyB === newBody) {
+            score += 10;
+            scoreElement.textContent = score;
+          }
+        }
+      });
+      
+
+
+
+
+
+      World.add(world, newBody);
     }
 
     if (
       !disableAction &&
       (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
-      alert("Game over");   
+      alert("Game over");
+
+
+
+      
     }
- }); 
+  });
 });
 
-  addFruit();
+addFruit();
+
+
